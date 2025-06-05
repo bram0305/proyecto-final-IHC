6b254a985c4304fb4704e603e49e91ff127b0a94
Luis Miguel Santiago Caraza <N00214419@upn.pe>
2025-06-04 23:07:32 -0500

    rachel

--- src/main/java/com/example/demo/controllers/PrincipalController.java
+++ src/main/java/com/example/demo/controllers/PrincipalController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class PrincipalController {
@@ -14,5 +17,22 @@ public class PrincipalController {
     @RequestMapping("/iniciosesion")
     public String iniciosesion(){
         return "iniciosesion";
+    }
+
+    @RequestMapping("/iniciosesion/registrar")
+    public String registrar(){
+        return "registrar";
+    }
+    
+    @PostMapping("/iniciosesion/registrar/validar")
+    public String datospersonales(@RequestParam("correo") String correo,@RequestParam("contra") String contra,@RequestParam("confirmacontra") String confirmacontra,@RequestParam("tele") String tele, RedirectAttributes flash){
+        String mensaje = "";
+        if(!correo.equals("") && !contra.equals("") && !confirmacontra.equals("") && !tele.equals("")){
+            mensaje = "Datos registrados correctamente";
+        }else{
+            mensaje = "Los datos no se han sido registrados";
+        }
+        flash.addFlashAttribute("mensaje", mensaje);
+        return "redirect:/registrar/";
     }
 }
